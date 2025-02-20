909d6531b2290216c0e56dee1ad84dbfe2493b85
Andrew Xing <andrewxing43@g.ucla.edu>
2025-02-19 19:11:28 -0800

    add some comments

--- nndl/naive_filter.py
+++ nndl/naive_filter.py
@@ -1,5 +1,9 @@
+"""
+Answer for question 11
+"""
+
+
 from scipy.sparse import coo_matrix
-import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
